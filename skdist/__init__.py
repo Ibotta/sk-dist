0ece6925a17e4939dcda5b2733129de001e218c3
Evan Harris <32207830+denver1117@users.noreply.github.com>
2019-08-14 15:23:42 -0600

    Create __init__.py

---
+++ skdist/__init__.py
@@ -0,0 +1,16 @@
+"""
+Distributed scikit-learn meta-estimators in PySpark
+===================================================
+skdist is a Python module that aims to efficiently distribute scikit-learn 
+meta-estimator model fitting with PySpark. Where sklearn uses joblib to 
+parallelize these operations, distml uses spark, enabling effectively infinite 
+parallelization, unconstrained by the number of cores on a single machine. 
+
+Existing scikit-learn meta-estimators for model selection, ensembling, 
+multiclass fitting, and feature unioning are inherited, and have their fit methods 
+overridden with a distributed implementation using spark. The resulting fitted model 
+is effectively identical to its single machine counterpart, 
+ready for prediction using the inherited methods.
+"""
+
+__version__ = '0.0.1'
