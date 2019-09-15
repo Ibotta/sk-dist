f8465262d0a5bc1e5e1351244df76e3db810ea77
Charley Frazier <charles.frazier1991@gmail.com>
2019-09-15 14:33:04 -0600

    add real skdist test

--- skdist/tests/test_spark.py
+++ skdist/tests/test_spark.py
@@ -1,6 +1,8 @@
-import pyspark
-import pytest
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_breast_cancer
 
+import pyspark
+from skdist.distribute.ensemble import DistRandomForestClassifier
 
 def test_spark_session_dataframe(spark_session):
     test_df = spark_session.createDataFrame([[1, 3], [2, 4]], "a: int, b: int")
@@ -15,3 +17,31 @@ def test_spark_session_sql(spark_session):
 
     test_filtered_df = spark_session.sql('SELECT a, b from test where a > 1')
     assert test_filtered_df.count() == 1
+
+
+def test_dist_fit_predict(spark_session):
+    sc = spark_session.sparkContext
+
+    test_size = 0.2
+    max_depth = None
+    n_estimators = 100
+
+    # load sample data (binary target)
+    data = load_breast_cancer()
+    X = data["data"]
+    y = data["target"]
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=test_size, random_state=10
+    )
+
+    ### distributed random forest
+    model = DistRandomForestClassifier(
+        n_estimators=n_estimators,
+        max_depth=max_depth, sc=sc,
+    )
+    # distributed fitting with spark
+    model.fit(X_train, y_train)
+    # predictions on the driver
+    preds = model.predict(X_test)
+
+    assert preds.shape == y_test.shape
