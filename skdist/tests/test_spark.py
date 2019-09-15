60472be08338654e935bdc959aeb8404e0de78dc
Charley Frazier <charles.frazier1991@gmail.com>
2019-09-15 14:38:07 -0600

    fix deprecation warning

--- skdist/tests/test_spark.py
+++ skdist/tests/test_spark.py
@@ -13,7 +13,7 @@ def test_spark_session_dataframe(spark_session):
 
 def test_spark_session_sql(spark_session):
     test_df = spark_session.createDataFrame([[1, 3], [2, 4]], "a: int, b: int")
-    test_df.registerTempTable('test')
+    test_df.createOrReplaceTempView('test')
 
     test_filtered_df = spark_session.sql('SELECT a, b from test where a > 1')
     assert test_filtered_df.count() == 1
