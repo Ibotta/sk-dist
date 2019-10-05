2bc4c397780d82ee6a1fa2362e78008e0d04691b
denver1117 <emitchellh@gmail.com>
2019-10-04 21:56:02 -0600

    Fix path

--- build_tools/spark_2_3.sh
+++ build_tools/spark_2_3.sh
@@ -4,8 +4,7 @@ mkdir -p /opt
 wget -q -O /opt/spark.tgz http://www.gtlib.gatech.edu/pub/apache/spark/spark-2.3.4/spark-2.3.4-bin-hadoop2.7.tgz
 tar xzf /opt/spark.tgz -C /opt/
 rm /opt/spark.tgz
-ls /opt/
-export SPARK_HOME=/opt/spark-2.3.4-bin-hadoop2.7.tgz
+export SPARK_HOME=/opt/spark-2.3.4-bin-hadoop2.7
 export PATH=$PATH:/opt/spark-2.3.4-bin-hadoop2.7/bin
 pip install -e .[tests]
 build_tools/test_script.sh
