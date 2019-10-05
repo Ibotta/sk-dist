5f5a08f1defb3311629f1a1accc1fdf324e4a128
denver1117 <emitchellh@gmail.com>
2019-10-04 21:45:48 -0600

    Add spark_2_3

---
+++ build_tools/spark_2_3.sh
@@ -0,0 +1,10 @@
+PATH=$(echo "$PATH" | sed -e 's/:\/usr\/local\/lib\/jvm\/openjdk11\/bin//')
+JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64
+mkdir -p /opt
+wget -q -O /opt/spark.tgz http://www.gtlib.gatech.edu/pub/apache/spark/spark-2.3.4/spark-2.3.4-bin-hadoop2.7.tgz
+tar xzf /opt/spark.tgz -C /opt/
+rm /opt/spark.tgz
+export SPARK_HOME=/opt/spark-2.3.4-bin-hadoop2.7.tgz
+export PATH=$PATH:/opt/spark-2.3.4-bin-hadoop2.7/bin
+pip install -e .[tests]
+build_tools/test_script.sh
