4b9811e1ead9ccd69bdd0aaf24429a37b988e702
Chad Foley <chad.foley@ibotta.com>
2019-10-07 15:07:25 -0600

    adding encoder test with tuple column

--- skdist/distribute/tests/test_encoder.py
+++ skdist/distribute/tests/test_encoder.py
@@ -60,3 +60,24 @@ def test_dict_list():
     encoderizer = encoder.Encoderizer(size="medium")
     X_t = encoderizer.fit_transform(df)
     assert X_t.shape == (12, 210)
+
+def test_dict_list_tuple():
+    # pandas encoderizer example
+    X = [
+        "[text encoding is fun]",
+        "this is a text encoding {'pizza': 'example'}",
+        "hopefully it works and [we'll have passed] the test"
+        ]
+    df = pd.DataFrame({
+	    "text_col": X*4,
+	    "categorical_str_col": ["control", "treatment", "control"]*4,
+	    "categorical_int_col": [0, 1, 2]*4,
+	    "numeric_col": [5, 22, 69]*4,
+	    "dict_col": [{"a": 4}, {"b": 1}, {"c": 3}]*4,
+	    "list_col": [[1, 2], [1, 3], [2,]]*4,
+	    "tuple_col": [(1, 2), (1, 3), (2,)]*4
+	    })
+    encoderizer = encoder.Encoderizer(size="medium")
+    X_t = encoderizer.fit_transform(df)
+    assert X_t.shape == (12, 244)
+    
