912e2707060854a13113b13486b743973b795527
Evan Harris <32207830+denver1117@users.noreply.github.com>
2020-01-11 19:50:43 -0700

    Add python versions to classifiers

--- setup.py
+++ setup.py
@@ -13,6 +13,10 @@ with open("README.rst") as f:
 CLASSIFIERS = [
     "Intended Audience :: Developers",
     "Programming Language :: Python",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
     "Topic :: Scientific/Engineering"
     ]
 AUTHOR = "Ibotta Inc."
