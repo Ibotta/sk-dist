4606f398969aad3074d6a64b7f76ebef1cdbb5df
denver1117 <emitchellh@gmail.com>
2020-02-10 10:20:27 -0700

    Try import pyarrow

--- skdist/distribute/predict.py
+++ skdist/distribute/predict.py
@@ -32,19 +32,12 @@ def _is_pyspark_installed():
         return False
 
 def _is_pyarrow_installed():
-    global _PYARROW_INSTALLED
-    if _PYARROW_INSTALLED is None:
-        try:
-            import pyarrow
-            _PYARROW_INSTALLED = True
-        except ImportError:
-            _PYARROW_INSTALLED = False
-
-    if _PYARROW_INSTALLED:
+    try:
         import pyarrow
         return True
-    else:
-        return False
+    except ImportError:
+        pass
+    return False
 
 def _get_vals(*cols, feature_type="numpy", names=None):
     """ Prep input data for prediction method """
