e5729e62fbdb7b8513be1c4fd0d463d8aec5b837
Evan Harris <32207830+denver1117@users.noreply.github.com>
2020-11-03 15:54:27 -0700

    Spark distribution source updates (#46)
    * Update spark_3_0.sh
    
    * Update spark_2_4.sh

--- build_tools/spark_2_4.sh
+++ build_tools/spark_2_4.sh
@@ -7,7 +7,7 @@ then
   PATH=$(echo "$PATH" | sed -e 's/:\/usr\/local\/lib\/jvm\/openjdk11\/bin//')
   JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64
   mkdir -p /opt
-  wget -q -O /opt/spark.tgz http://www.gtlib.gatech.edu/pub/apache/spark/spark-2.4.7/spark-2.4.7-bin-hadoop2.7.tgz
+  wget -q -O /opt/spark.tgz https://mirrors.ocf.berkeley.edu/apache/spark/spark-2.4.7/spark-2.4.7-bin-hadoop2.7.tgz
   tar xzf /opt/spark.tgz -C /opt/
   rm /opt/spark.tgz
   export SPARK_HOME=/opt/spark-2.4.7-bin-hadoop2.7
--- build_tools/spark_3_0.sh
+++ build_tools/spark_3_0.sh
@@ -2,7 +2,7 @@
 PATH=$(echo "$PATH" | sed -e 's/:\/usr\/local\/lib\/jvm\/openjdk11\/bin//')
 JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64
 mkdir -p /opt
-wget -q -O /opt/spark.tgz http://www.gtlib.gatech.edu/pub/apache/spark/spark-3.0.1/spark-3.0.1-bin-hadoop2.7.tgz
+wget -q -O /opt/spark.tgz https://mirrors.gigenet.com/apache/spark/spark-3.0.1/spark-3.0.1-bin-hadoop2.7.tgz
 tar xzf /opt/spark.tgz -C /opt/
 rm /opt/spark.tgz
 export SPARK_HOME=/opt/spark-3.0.1-bin-hadoop2.7
