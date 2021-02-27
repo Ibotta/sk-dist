231a1a406859748555819f5aba0f278328fde6a9
Evan Harris <32207830+denver1117@users.noreply.github.com>
2021-02-26 18:22:17 -0700

    bump spark version

--- build_tools/spark_3_0.sh
+++ build_tools/spark_3_0.sh
@@ -2,11 +2,11 @@
 PATH=$(echo "$PATH" | sed -e 's/:\/usr\/local\/lib\/jvm\/openjdk11\/bin//')
 JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64
 mkdir -p /opt
-wget -q -O /opt/spark.tgz https://mirrors.gigenet.com/apache/spark/spark-3.0.1/spark-3.0.1-bin-hadoop2.7.tgz
+wget -q -O /opt/spark.tgz https://mirrors.gigenet.com/apache/spark/spark-3.0.2/spark-3.0.2-bin-hadoop2.7.tgz
 tar xzf /opt/spark.tgz -C /opt/
 rm /opt/spark.tgz
-export SPARK_HOME=/opt/spark-3.0.1-bin-hadoop2.7
-export PATH=$PATH:/opt/spark-3.0.1-bin-hadoop2.7/bin
+export SPARK_HOME=/opt/spark-3.0.2-bin-hadoop2.7
+export PATH=$PATH:/opt/spark-3.0.2-bin-hadoop2.7/bin
 
 # run tests
 pip install -e .[tests]
