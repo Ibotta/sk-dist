f1443d6d39b0365f2d536d0a3c3796bcb7f23892
Bobby Crimi <rpcrimi@gmail.com>
2021-06-07 10:04:22 -0600

    dont use tar xzf

--- build_tools/spark_3_0.sh
+++ build_tools/spark_3_0.sh
@@ -3,7 +3,7 @@ PATH=$(echo "$PATH" | sed -e 's/:\/usr\/local\/lib\/jvm\/openjdk11\/bin//')
 JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64
 mkdir -p /opt
 wget -q -O /opt/spark.tgz https://mirrors.gigenet.com/apache/spark/spark-3.0.2/spark-3.0.2-bin-hadoop2.7.tgz
-tar xf /opt/spark.tgz -C /opt/
+tar xzf /opt/spark.tgz -C /opt/
 rm /opt/spark.tgz
 export SPARK_HOME=/opt/spark-3.0.2-bin-hadoop2.7
 export PATH=$PATH:/opt/spark-3.0.2-bin-hadoop2.7/bin
