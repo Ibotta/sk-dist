5ee99d0b28097ff5809c50cd069b06cbe29c4b46
Bobby Crimi <rpcrimi@gmail.com>
2021-06-07 10:29:39 -0600

    revert to previous format

--- build_tools/spark_2_4.sh
+++ build_tools/spark_2_4.sh
@@ -7,11 +7,8 @@ then
   PATH=$(echo "$PATH" | sed -e 's/:\/usr\/local\/lib\/jvm\/openjdk11\/bin//')
   JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64
   mkdir -p /opt
-  echo "downloading spark"
-  wget -q -O /opt/spark.tgz https://mirrors.gigenet.com/apache/spark/spark-2.4.7/spark-2.4.7-bin-hadoop2.7.tgz
-  echo "before untar"
-  tar xzvf /opt/spark.tgz -C /opt/
-  echo "after untar"
+  wget -q -O /opt/spark.tgz https://mirrors.ocf.berkeley.edu/apache/spark/spark-2.4.7/spark-2.4.7-bin-hadoop2.7.tgz
+  tar xzf /opt/spark.tgz -C /opt/
   rm /opt/spark.tgz
   export SPARK_HOME=/opt/spark-2.4.7-bin-hadoop2.7
   export PATH=$PATH:/opt/spark-2.4.7-bin-hadoop2.7/bin
